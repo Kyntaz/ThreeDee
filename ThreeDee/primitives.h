1cdda888e2ebbfaa60b2c2f28cd4d346465dd96d
Kyntaz <ghost.615b@gmail.com>
2019-02-26 18:48:44 +0000

    add sphere constructor

--- ThreeDee/primitives.h
+++ ThreeDee/primitives.h
@@ -7,12 +7,15 @@ typedef struct _collision Collision;
 
 class Primitive {
 	public:
-		virtual Collision intersect(Ray ray) = 0;
+	virtual Collision intersect(Ray ray) = 0;
 };
 
 class Sphere : public Primitive {
 	public:
 	float radius;
+
+	Sphere(float r) : radius(r) {};
+
 	Collision intersect(Ray ray) {
 		//TODO: make it happen!
 		return {};
