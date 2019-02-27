a3ecd850721ae01ca1cb675dedb3fa134dd1c31b
PedroCaldeira <pedromiguelcc@tecnico.ulisboa.pt>
2019-02-27 00:24:01 +0000

    Implement Sphere NFF parsing| Add test file

--- ThreeDee/scene.h
+++ ThreeDee/scene.h
@@ -4,18 +4,15 @@
 #include <fstream>
 #include <sstream>
 #include <stdio.h>
+#include <vector>
+#include "primitives.h"
 class Scene
 {
 
 public:
 
-	Scene()
-	{
-	}
-
-	~Scene()
-	{
-	}
+	Scene(){}
+	
 	bool load_nff(const char* filename) {
 
 		std::ifstream inputFileStream(filename);
@@ -24,10 +21,18 @@ public:
 		std::string line;
 		std::string command;
 		while (std::getline(inputFileStream, line)) {
-
+			
 			std::istringstream lineStream(line);
+			std::vector<std::string> tokens;
 			while (std::getline(lineStream, command, ' ')) {
-				std::cout << command << std::endl;
+				tokens.push_back(command);
+			}
+			command = tokens[0];
+			if (command=="s") {
+				Sphere* sphere = new Sphere(std::stof(tokens[1]), 
+											std::stof(tokens[2]), 
+											std::stof(tokens[3]), 
+											std::stof(tokens[4]));
 			}
 		}
 		return true;
