3eb2a8a615d277a9074662141b2e1af1abc4f8fd
Kyntaz <ghost.615b@gmail.com>
2019-04-02 17:18:46 +0100

    fix some code

--- ThreeDee/utils.h
+++ ThreeDee/utils.h
@@ -3,3 +3,9 @@
 float generateRandom(float minValue, float maxValue) {
 	return minValue + (maxValue - minValue) * ((float)rand() / RAND_MAX);
 }
+
+float clamp(float v, float lo, float hi) {
+	if (v < lo) return lo;
+	if (v > hi) return hi;
+	return v;
+}
